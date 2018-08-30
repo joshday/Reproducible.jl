3c06e8beda5349ccb8b6bac9fe3a27e4eeffc8cb
Josh Day <emailjoshday@gmail.com>
2018-08-30 16:44:28 -0400

    wip

--- example/index.md
+++ example/index.md
@@ -4,6 +4,10 @@ builddir = "/Users/joshday/.julia/dev/Reproducible/example/build"
 
 # Example on using Reproducible.jl
 
+Here is some math:
+
+\$\$ f(x) = x ^ 2 \$\$
+
 ## Getting Started
 
 This code block uses `julia`, and does not get eval-ed
@@ -14,7 +18,14 @@ y = 2
 ```
 Compare that with this:
 
-```julia; repl; out=string;
+```julia; repl;
 x = 1 
 y = 2
 ```
+
+## Markdown Stuff
+
+| LJ | RJ |
+|:---|---:|
+| 1  | 1  |
+| 2  | 2  |
--- src/Reproducible.jl
+++ src/Reproducible.jl
@@ -2,19 +2,26 @@ module Reproducible
 
 using Markdown
 
-function build(path::String, buildpath::String = joinpath(dirname(path), "build"))
-    s = read(path, String)
+function build(path::String, buildpath::String = joinpath(dirname(path), "build"); 
+        to = :html, 
+        style = joinpath(@__DIR__(), "..", "styles", "github.css"),
+        css = "https://raw.githubusercontent.com/sindresorhus/github-markdown-css/gh-pages/github-markdown.css")
     mod = Main.eval(:(module __Temp__ end))
-    s2 = @eval mod $s
-    md = Markdown.parse(s2)
+    @eval mod using Reproducible
+    md = Markdown.parse(read(path, String))
     isdir(buildpath) && rm(buildpath; recursive=true, force=true)
-    open(touch(joinpath(mkdir(buildpath), basename(path))), "w") do io
+    file = touch(joinpath(mkdir(buildpath), basename(path)))
+    open(file, "w") do io
         for x in md.content
             write(io, isa(x, Markdown.Code) ? code2string(x, mod) : Markdown.plain(x))
             write(io, '\n')
         end
     end
+    output = file[1:(end-3)] * ".$to"
+    run(`pandoc --from markdown --katex --to $to $file -o $output --css $css`)
 end
+
+
 
 # parse and eval `code` into `mod`
 function eval_in(code::String, mod::Module)
@@ -36,6 +43,15 @@ end
     # collapse    # should source and return value go in the same markdown code block?
     # custom      # f(code::String, mod::Module) that overwrites all other options
 
+function repl(code, mod)
+    out = eval_in(code, mod)
+    s = "```"
+    for outi in out 
+        s *= "\njulia> $(strip(outi[1]))\n$(outi[2])\n"
+    end
+    s * "```\n"
+end
+
 
 #-----------------------------------------------------------------------# code2string
 function code2string(o::Markdown.Code, mod::Module)
@@ -46,9 +62,7 @@ function code2string(o::Markdown.Code, mod::Module)
         if blocktype.args[1] != :julia
             return Markdown.plain(o)
         else
-            @show blocktype.args
-            # FIXME
-            return ""
+            repl(o.code, mod)
         end
     end
 end
