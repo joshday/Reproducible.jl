04ea6d3530ac081d118d9726bf8f10ef1154f0b9
Josh Day <emailjoshday@gmail.com>
2018-08-29 20:12:12 -0400

    remove commented code

--- src/Reproducible.jl
+++ src/Reproducible.jl
@@ -53,48 +53,4 @@ function code2string(o::Markdown.Code, mod::Module)
     end
 end
 
-
-
-# #-----------------------------------------------------------------------# juliahide
-# function juliahide(o, mod) 
-#     runcode(o, mod)
-#     return "<!-- $(rstrip(Markdown.plain(o))) -->\n"
-# end
-
-# #-----------------------------------------------------------------------# juliablock
-# function juliablock(o, mod) 
-#     out = runcode(o, mod)
-#     s = Markdown.plain(o)
-#     if o.code[end] != ';' 
-#         s *= "\n```\n$(out[end])\n```\n"
-#     end
-#     return s
-# end
-
-# #-----------------------------------------------------------------------# juliarepl
-# function juliarepl(o, mod)
-#     s = "```\n"
-#     n = 1 
-#     while n < length(o.code)
-#         nold = n
-#         ex, n = Meta.parse(o.code, n)
-#         val = @eval(mod, $ex)
-#         s *= "julia> $(o.code[nold:n-1])"
-#         n > length(o.code) && (s *= "\n")
-#         s *= "$val"
-#         n < length(o.code) && (s *= "\n\n")
-#     end
-#     s *= "\n```"
-#     return s
-# end
-
-# blockfunction(::Val) = (code, mod) -> ""
-# blockfunction(::Val{:juliablock}) = juliablock
-# blockfunction(::Val{:juliarepl})  = juliarepl
-# blockfunction(::Val{:juliahide})  = juliahide
-
-# function code2string(code::Markdown.Code, mod::Module)
-#     blockfunction(Val(Symbol(split(code.language)...)))(code, mod)
-# end
-
 end # module
