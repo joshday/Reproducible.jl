f65386508733a41e601ddde500e9765b785ad143
Josh Day <emailjoshday@gmail.com>
2018-09-20 12:26:44 -0400

    toc stuff

--- src/toc.jl
+++ src/toc.jl
@@ -4,7 +4,7 @@ function maketoc(path::String; depth=2, title = "# Table of Contents")
         for level in 1:depth
             if isa(item, Markdown.Header{level}) 
                 header = strip(replace(Markdown.plain(item), "#" => ""))
-                link = replace(header, " " => "_")
+                link = lowercase(replace(header, " " => "-"))
                 s *= repeat(' ', (level-1)*4) * "- [$header](#$link)\n"
             end
         end
