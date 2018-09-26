aa98d2bd6c6edbb67591629e5247afc66a7c05d5
Josh Day <emailjoshday@gmail.com>
2018-09-26 05:35:33 -0400

    actually fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,9 @@ end
 
 @testset "CodeBlock" begin
     cb = CodeBlock("x = 1\ny = 2", TestMod)
-    @test strip(cb[1][1]) == "x = 1"
+    @test strip(cb[1].input) == "x = 1"
+    @test strip(cb[2].input) == "y = 2"
     @test cb[1].output == 1
-    @test cb[2].input == "y = 2"
     @test cb[2].output == 2
 end
 
