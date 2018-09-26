439f48b1db193919f18bcb288f393a2d5254acf2
Josh Day <emailjoshday@gmail.com>
2018-09-26 05:32:21 -0400

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,8 +8,8 @@ end
 @testset "CodeBlock" begin
     cb = CodeBlock("x = 1\ny = 2", TestMod)
     @test strip(cb[1][1]) == "x = 1"
-    @test cb[1][2] == 1
-    @test cb[2][1] == "y = 2"
-    @test cb[2][2] == 2
+    @test cb[1].output == 1
+    @test cb[2].input == "y = 2"
+    @test cb[2].output == 2
 end
 
